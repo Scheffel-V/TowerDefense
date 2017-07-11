977d8fe1a5d81a7d263c67b1bab34acf7cb60cfd
felipeleivas <leivascmpa@hotmail.com>
2017-07-11 01:11:07 -0300

    Foi

--- TryFunc/mapcreator.py
+++ TryFunc/mapcreator.py
@@ -1,39 +1,33 @@
 import config
 import pygame
 import sys
-import map
+import map as map2
 import rectangle
 from sys import argv
 import os
 from func import *
 
-
 class MapCreator:
-	def __init__(self, filemap=None):		
+	def __init__(self, filemap=None):
 		self._conf = config.Config()
 		self._screen = pygame.display.set_mode(self._conf.DISPLAY_SIZE)
-		self._matrix = [[0 for aux in range(self._conf.MAP_DIMX)] for aux2 in range(self._conf.MAP_DIMY)]
-		self._rectMap = map.Map2(self._conf.MAP_DIMS, self._conf.RECT_DIMS_px, self._matrix, 'creation')
+		self._matrix = []
+		self.inicializeMatrix2PerLines(self._conf.MAP_DIMX, 0, self._conf.MAP_DIMY)
+		self._rectMap = map2.Map2(self._conf.MAP_DIMS, self._conf.RECT_DIMS_px, self._matrix, 'creation')
 		self._currentValue = 1
 		self._filemap = filemap
-		#self._matrix = self.createMatrixWithZeros(self._conf.MAP_DIMX, self._conf.MAP_DIMY)
-
-##----TENTATIVA FRACASSADA----------------------------------------------------------------
-	def createMatrixWithZeros(self, x, y):
-		return self.createMatrixColumn(self.createMatrixLane(x), y)
-
-	def createMatrixLane(self, x):
-		if x == 0:
-			return []
+	
+	def inicializeMatrix2PerLines(self,max_X,x,max_Y):
+		if not (x == max_X):
+			self._matrix.append(self.inicializeMatrix2PerColumns(0,max_Y))
+			self.inicializeMatrix2PerLines(max_X,x+1,max_Y)
+	
+	def inicializeMatrix2PerColumns(self,y,max_Y):
+		if not (y == max_Y):
+			return cons(0, self.inicializeMatrix2PerColumns(y+1,max_Y))
 		else:
-			return cons(0, self.createMatrixLane(x-1))
+			return empty
 
-	def createMatrixColumn(self, lane, y):
-		if y == 0:
-			return []
-		else:
-			return cons(lane, self.createMatrixColumn(lane,y-1))
-##--------------------------------------------------------------------
 	def getCurrentValue(self):
 		return self._currentValue
 
@@ -62,7 +56,9 @@ class MapCreator:
 	def setMatrixElement(self,coords,value=0):
 		try:
 			if self._isValidValue(value):
-				self._matrix[second(coords)][first(coords)] = value
+				x = coords[1]
+				y = coords[0]
+				self._matrix[x][y] = value
 			else:
 				raise ValueError("Invalid value !")
 		except IndexError:
@@ -84,34 +80,36 @@ class MapCreator:
 		pygame.display.set_caption(string)
 
 	def getClickedSquare(self, pxPosition):
-		xPos = int(first(pxPosition)/self.getConf().RECT_DIMX_px)
-		yPos = int(second(pxPosition)/self.getConf().RECT_DIMX_px)
+		xPos = int(pxPosition[0]/self.getConf().RECT_DIMX_px)
+		yPos = int(pxPosition[1]/self.getConf().RECT_DIMX_px)
 		if xPos < self.getConf().MAP_DIMX and yPos < self.getConf().MAP_DIMY:	
 			return (xPos, yPos)
 		pass
 
-	def standardizeName (self, maps):
-		if is_empty(maps):
-			return []
-		else:
-			mapa = first(maps)
-			mapa = mapa.split('/')[-1]
-			mapa = int(mapa.split('.')[0][3:])
-			return cons(mapa, self.standardizeName(rest(maps)) )
-
 	def getNewFileName(self):
+
 		if self._filemap != None:
 			return self._filemap
 
 		maps = os.listdir("maps")
-		if is_empty(maps):
+
+		if maps == []:
 			return 'maps/map1.map'
 
-		all_index = self.standardizeName(maps)
+		all_index = self.getNewFileName2(maps)
 
 		return 'maps/map' + str(max(all_index)+1) + '.map'
 
+	def getNewFileName2(self,maps):
+		if(not(is_empty(maps))):
+			mapa = first(maps)
+			name = mapa.split('/')[-1]
+			index = int(name.split('.')[0][3:])
+			return cons(index,self.getNewFileName2(rest(maps)))
+		else:
+			return empty
 	def saveMatrix(self):
+
 		try:
 			filename = 'maps/' + argv[1]
 		except IndexError:
@@ -120,9 +118,7 @@ class MapCreator:
 		print("Saving map as " + filename)
 		file = open(filename, 'w') 
 
-		for line in self.getMatrix():
-			file.write(str(line)+'\n')
-
+		list(map(lambda line: file.write(str(line)+'\n'),list(self.getMatrix())))
 		file.close()
 
 	def loadMap(self, player_map):
@@ -133,38 +129,70 @@ class MapCreator:
 
 		f = open(player_map)
 		self._matrix = []
-		for line in f.readlines():
-			self._matrix.append([int(x) for x in line.strip('[]\n').split(',')])
+		file_lines = f.readlines()
+		self.inicializeMatrixPerLines(file_lines)	
+	def inicializeMatrixPerLines(self,matrix):
+		if not (is_empty(matrix)):
+			self._matrix.append(self.inicializeMatrixPerColumns(first(matrix).strip('[]\n').split(',')))
+			self.inicializeMatrixPerLines(rest(matrix))
+	def inicializeMatrixPerColumns(self,line):
+		if not (is_empty(line)):
+			return cons(int(first(line)), self.inicializeMatrixPerColumns(rest(line)))
+		else:
+			return empty
+        
 
+	#TODO: @Otavio -> Tirar as coisas hardcoded
 	def _handleMenuClick(self, pos):
 		if self.getClickedSquare(pos) == None: #ou seja, se nenhum dos quadrados na tela foi apertado...
-			if second(pos) < 372 and second(pos) > 328:
-				if first(pos) < 537 and first(pos) > 508:
+			if pos[1] < 372 and pos[1] > 328:
+				if pos[0] < 537 and pos[0] > 508:
 					self.setCurrentValue(self.getConf().MAP_NUMBMATRIX_PATH)
-				elif first(pos) < 617 and first(pos) > 583:
+				elif pos[0] < 617 and pos[0] > 583:
 					self.setCurrentValue(self.getConf().MAP_NUMBMATRIX_CENTRALPATH)
-				elif first(pos) < 692 and first(pos) > 655:
+				elif pos[0] < 692 and pos[0] > 655:
 					self.setCurrentValue(self.getConf().MAP_NUMBMATRIX_SPAWN)
-				elif first(pos) < 767 and first(pos) > 727:
+				elif pos[0] < 767 and pos[0] > 727:
 					self.setCurrentValue(self.getConf().MAP_NUMBMATRIX_CHANGEDIRECTION)
-			elif second(pos) < 458 and second(pos) > 409:
-				if first(pos) < 537 and first(pos) > 508:
+			elif pos[1] < 458 and pos[1] > 409:
+				if pos[0] < 537 and pos[0] > 508:
 					self.setCurrentValue(self.getConf().MAP_NUMBMATRIX_DESPAWN)
 
 	def _isValidValue(self,value):
 		if value in self.getConf().MAP_NUMBMATRIX_VALUES:
 			return True
-		else:
-			return False
+		return False
 
 	def _updateRectMap(self):
-		self.setRectMap(map.Map2(self.getConf().MAP_DIMS, self.getConf().RECT_DIMS_px, self._matrix, 'creation'))
-		for i in range(0, self.getRectMap().getDimension()[0]):
-			for j in range(0, self.getRectMap().getDimension()[1]):
-				self.getRectMap().getMap()[i][j][1].paint(self.getScreen())
+		self.setRectMap(map2.Map2(self.getConf().MAP_DIMS, self.getConf().RECT_DIMS_px, self._matrix, 'creation'))
+		self.paintRectMap(self.getScreen())
+
+	def paintRectMap(self, gameDisplay):
+		matrix = self.getRectMap().getMap()
+		self.paintRectMapPerLine(matrix,gameDisplay)
+
+
+	def paintRectMapPerLine(self,matrix,gameDisplay):
+		if not (is_empty(matrix)):
+			line = list(first(matrix))
+			self.paintRectMapPerColumn(line,gameDisplay)
+			self.paintRectMapPerLine(rest(matrix),gameDisplay)
+		else:
+			pass
+
+	def paintRectMapPerColumn(self,line,gameDisplay):
+		if not (is_empty(line)):
+			second(first(line)).paint(gameDisplay)
+			self.paintRectMapPerColumn(rest(line),gameDisplay)
+	
+
+
+
+
 
 	def _updateScreenBuffer(self):
 		self._updateRectMap()
+		#TODO: More things in here
 
 	def _show(self):
 		pygame.display.update()
@@ -192,33 +220,42 @@ class MapCreator:
 		isLeftClicked = False
 		isRightClicked = False
 
-		while True:
-			mousePos = pygame.mouse.get_pos()
-			if isLeftClicked:
-				self.setMatrixElement(self.getClickedSquare(mousePos),value=self.getCurrentValue())
+		self.goFunctional()
+	def goFunctional(self):
+		
+		mousePos = pygame.mouse.get_pos()
+		if isLeftClicked:
+			self.setMatrixElement(self.getClickedSquare(mousePos),value=self.getCurrentValue())
 
-			elif isRightClicked:
-				self.setMatrixElement(self.getClickedSquare(mousePos),value=0)
+		elif isRightClicked:
+			self.setMatrixElement(self.getClickedSquare(mousePos),value=0)
 
-			event = pygame.event.poll() # same as 'for event in pygame.event.get():' but more elegant
+		event = pygame.event.poll() # same as 'for event in pygame.event.get():' but more elegant
 
-			if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
-				self.saveMatrix()
-				sys.exit("Exiting Map Creation !")
+		if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+			self.saveMatrix()
+			sys.exit("Exiting Map Creation !")
 
-			if event.type == pygame.MOUSEBUTTONDOWN and event.button == self.getConf().LEFT_BUTTON:
-				isLeftClicked = True
-				self._handleMenuClick(mousePos)
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button == self.getConf().LEFT_BUTTON:
+			isLeftClicked = True
+			self._handleMenuClick(mousePos)
 
-			if event.type == pygame.MOUSEBUTTONUP and event.button == self.getConf().LEFT_BUTTON:
-				isLeftClicked = False
+		if event.type == pygame.MOUSEBUTTONUP and event.button == self.getConf().LEFT_BUTTON:
+			isLeftClicked = False
 
-			if event.type == pygame.MOUSEBUTTONDOWN and event.button == self.getConf().RIGHT_BUTTON:
-				isRightClicked = True
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button == self.getConf().RIGHT_BUTTON:
+			isRightClicked = True
 
-			if event.type == pygame.MOUSEBUTTONUP and event.button == self.getConf().RIGHT_BUTTON:
-				isRightClicked = False
+		if event.type == pygame.MOUSEBUTTONUP and event.button == self.getConf().RIGHT_BUTTON:
+			isRightClicked = False
 
-			self._updateScreenBuffer()
-			self._show()
+		self._updateScreenBuffer()
+		self._show()
+		self.goFunctional()
+
+
+# if __name__ == '__main__':
+# 	mapcreator = MapCreator()
+# 	mapcreator.setWindowCaption("Map Creation Window !")
+# 	mapcreator.start()
 	
